fb0c8ffc34bfa4b6399a5c44394fa37d491b562f
Krishna Karthik <krishnakart209@gmail.com>
2026-07-04 12:19:01 +0530

    Time: 4 ms (64.02%), Space: 19.4 MB (19.27%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -4,7 +4,7 @@ class Solution:
             "I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000
         }
         n=len(s)
-        total=roman[s[-1]]
+        total=roman[s[n-1]]
         for i in range(n-2,-1,-1):
             if roman[s[i]]< roman[s[i+1]]:
                 total-=roman[s[i]]
