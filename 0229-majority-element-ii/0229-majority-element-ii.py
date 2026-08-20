4afa37427b67a0c4e6ea2606ef25f8206c6eab51
Krishna Karthik <krishnakart209@gmail.com>
2026-08-20 15:16:08 +0530

    Time: 9 ms (43.26%), Space: 22.7 MB (51.7%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -2,14 +2,35 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         n=len(nums)
         res=[]
-        h1={}
+        count1=0
+        count2=0
+        el1=float("-inf")
+        el2=float("inf")
         for i in range(0,n):
-            if nums[i] in h1:
-                h1[nums[i]]+=1
+            if count1==0 and nums[i]!=el2:
+                count1=1
+                el1=nums[i]
+            elif count2==0 and nums[i]!=el1:
+                count2=1
+                el2=nums[i]
+            elif nums[i]==el1:
+                count1+=1
+            elif nums[i]==el2:
+                count2+=1
             else:
-                h1[nums[i]]=1
-        for j in h1:
-            if h1[j]>n//3:
-                res.append(j)
+                count1-=1
+                count2-=1
+        count1=0
+        count2=0
+        for i in range(0,n):
+            if nums[i]==el1:
+                count1+=1
+            if nums[i]==el2:
+                count2+=1
+        if count1>(n//3):
+            res.append(el1)
+        if count2>(n//3):
+            res.append(el2)
         return res
-        
+
+
