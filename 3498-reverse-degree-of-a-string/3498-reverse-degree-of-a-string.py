bfe72c34d7691a565d6ab7ba7b2650b871d5e7c0
Krishna Karthik <krishnakart209@gmail.com>
2026-09-20 13:07:04 +0530

    Time: 3 ms (97.65%), Space: 19.2 MB (55.57%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -1,13 +1,10 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        d={'a':26,'b':25,'c':24,'d':23,'e':22,'f':21,'g':20,'h':19,'i':18,'j':17,'k':16,'l':15,
-        'm':14,'n':13,'o':12,'p':11,'q':10,'r':9,'s':8,'t':7,'u':6,'v':5,'w':4,
-        'x':3,'y':2,'z':1}
         n=len(s)
         total=0
         for i in range(n):
             indst=i+1
-            product=indst*d[s[i]]
-            total+=product
+            reverseind=26+ord('a')-ord(s[i])
+            total+=indst*reverseind
         return total
         
